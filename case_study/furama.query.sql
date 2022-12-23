4638e8c7274343a5efef638f8199c7d69ec9bfda
hoangthang1003 <110215092+hoangthang1003@users.noreply.github.com>
2022-12-23 21:22:58 +0700

    nopbai

--- case_study/furama.query.sql
+++ case_study/furama.query.sql
@@ -113,6 +113,32 @@ group by dvdk.ma_dich_vu_di_kem
 having so_luong = "1"
 order by ma_hop_dong;
 
--- iển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten, ten_trinh_do, ten_bo_phan, 
+-- hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten, ten_trinh_do, ten_bo_phan, 
 -- so_dien_thoai, dia_chi mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.
-select nv.ma_nhan_vien , nv.ho_ten , td.ma_trinh_do bp.ten_bo_phan , nv.so_dien_thoai , nv.dia_chi;
+select nv.ma_nhan_vien , nv.ho_ten , td.ma_trinh_do ,bp.ten_bo_phan , nv.so_dien_thoai , nv.dia_chi , count(nv.ma_nhan_vien) as so_luong
+from nhan_vien as nv
+join trinh_do as td on td.ma_trinh_do = nv.ma_trinh_do
+join bo_phan as bp on bp.ma_bo_phan = nv.ma_bo_phan
+join hop_dong as hd on hd.ma_nhan_vien = nv.ma_nhan_vien
+where year(hd.ngay_bat_dau) in (2020,2021)
+group by nv.ma_nhan_vien
+having so_luong <= 3;
+
+-- 16.	Xóa những Nhân viên chưa từng lập được hợp đồng nào từ năm 2019 đến năm 2021.
+SET SQL_SAFE_UPDATES = 0;
+delete from nhan_vien where nhan_vien.ma_nhan_vien not in (select ma_nhan_vien from hop_dong as hd
+where year(hd.ngay_bat_dau) in (2019,2020,2021) group by ma_nhan_vien);
+select * from nhan_vien;
+
+-- 17.	Cập nhật thông tin những khách hàng có ten_loai_khach từ Platinum lên Diamond, chỉ cập nhật những khách hàng đã từng đặt phòng với 
+-- Tổng Tiền thanh toán trong năm 2021 là lớn hơn 10.000.000 VNĐ. 
+
+select kh.*, sum(dv.chi_phi_thue + hdct.so_luong * dvdk.gia) as tong_tien from khach_hang as kh 
+join loai_khach as lkh on lkh.ma_loai_khach = kh.ma_loai_khach_hang
+join hop_dong as hd on  hd.ma_khach_hang = kh.ma_khach_hang
+join dich_vu as dv on hd.ma_dich_vu = dv.ma_dich_vu
+join hop_dong_chi_tiet as hdct on hdct.ma_hop_dong = hd.ma_hop_dong
+join dich_vu_di_kem as dvdk on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+where lkh.ten_loai_khach = 'Platinum'
+group by hd.ma_khach_hang 
+having tong_tien > 10000000;
