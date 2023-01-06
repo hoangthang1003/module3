55c6315b04f56806f43d63d995f1acb2db7ba71d
hoangthang1003 <110215092+hoangthang1003@users.noreply.github.com>
2023-01-06 23:00:02 +0700

    nopbai

---
+++ ss14/ex/src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String ketqua =  phuongTrinhBac2(2,1,-1);
+        System.out.println(ketqua);
+    }
+
+    public static String phuongTrinhBac2(int a,int b,int c) {
+        double delta = Math.pow(b, 2) - 4 * a * c;
+        double x1 ;
+        double x2 ;
+        if (delta < 0) {
+            System.out.println("Phương trình này không có nghiệm!");
+        } else if (delta == 0) {
+            x1 = (-b / (2 * a));
+            return "Phương trình bậc 2 này có 1 nghiệm là x1 = x2 = " + x1;
+        } else {
+            x1 = (-b + Math.sqrt(delta)) / (2 * a);
+            x2 = (-b - Math.sqrt(delta)) / (2 * a);
+            return "Phương trình bậc 2 này có 2 nghiệm x1 = " + x1 + " và x2 = " + x2;
+        }
+        return null;
+
+    }
+}
